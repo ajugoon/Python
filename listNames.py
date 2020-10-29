c78ce2981ea8c9dfbe4c9e283314d821c4c28fca
ajugoon <54783263+ajugoon@users.noreply.github.com>
2020-10-28 23:57:29 -0400

    appends 5 names to a list using a 'for' loop

---
+++ listNames.py
@@ -0,0 +1,18 @@
+# This is a program that adds 5 names to an empty list
+# Author: Mr. Jugoon
+# UCC October 29th, 2020
+
+# This is where we create the "empty" list
+names_list = [] #no contents at the current time 
+
+for position in range (0, 5): 
+
+    myname = str(input("Please enter a name: \n")) # input from keyboard
+
+    print("The name you entered is: " + myname) # shows the name to the user
+
+    # names_list[position] = myname # keeps updating the list
+    
+    names_list.append(str(myname)) # keeps updating the list
+
+    print(names_list)
