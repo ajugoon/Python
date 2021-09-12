8ebd53a4372775264e6809c758db73897c5108ba
ajugoon <54783263+ajugoon@users.noreply.github.com>
2021-09-12 17:49:23 -0400

    Update age.py

--- intro/age.py
+++ intro/age.py
@@ -3,5 +3,5 @@
 name = input("What's your name? ")
 print("Nice to meet you " + name + "!")
 year = input("What year were you born? ")
-age = 2020 - int(year)
+age = 2021 - int(year)
 print("You are " + str(age) + " years old, " + name + "!")
